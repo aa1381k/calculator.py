daa76637f992172483ca47ecb568a184480109dc
ali abdoli <a.abdoli.k81@gmail.com>
2020-09-23 21:23:51 +0330

    easy calcuator

--- calculator.py
+++ calculator.py
@@ -1,35 +1,35 @@
+
+def jam ():
+
+    jam_doo_addad = addad_aval + addad_dovom
+
+    print(jam_doo_addad)
+
+def manfi ():
+
+    manfi_doo_addad = addad_aval - addad_dovom
+
+    print(manfi_doo_addad)
+
+def zarb ():
+
+    zarb_doo_addad = addad_aval * addad_dovom
+
+    print(zarb_doo_addad)  
+
+def tagsim ():
+
+    tagsim_doo_addad = addad_aval / addad_dovom
+
+    print(tagsim_doo_addad)
+
 while 1:
 
-    addad_aval=int(input("enter first number: "))
+    addad_aval=float(input("enter first number : "))
 
-    alamat=input("alamat ra vared konid: ")
+    alamat=input("enter your Operator : ")
 
-    addad_dovom=int(input("enter second number: "))
-
-    def jam ():
-
-        jam_doo_addad = addad_aval + addad_dovom
-
-        print(jam_doo_addad)
-
-    def manfi ():
-
-        manfi_doo_addad = addad_aval - addad_dovom
-
-        print(manfi_doo_addad)
-
-    def zarb ():
-
-        zarb_doo_addad = addad_aval * addad_dovom
-
-        print(zarb_doo_addad)  
-
-    def tagsim ():
-
-        tagsim_doo_addad = addad_aval / addad_dovom
-
-        print(tagsim_doo_addad)
-
+    addad_dovom=float(input("enter second number : "))
 
     if alamat == "+":
 
